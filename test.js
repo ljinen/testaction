35d7b05d7f59044793ec62137740d6665abcb14e
lje024 <60819403+lje024@users.noreply.github.com>
2021-02-18 16:58:00 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const { chromium } = require('playwright');
+(async () => {
+	const browser = await chromium.launch({headless: false});
+	const context = await browser.newContext();
+	const page = await context.newPage();
+	await page.goto('https://www.ipify.org/');
+	let IP = await page.innerText('body > section.about-slide > div.content-in-blocks > div > div.pre-wrapper.self-ipv4-block > pre > code > span:nth-child(5)');
+	console.log(IP);
+	await browser.close();
+})();
