c39d1331bf662fbbeae8996dcb297bfe92d1fa85
lje024 <60819403+lje024@users.noreply.github.com>
2021-02-18 18:45:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { chromium } = require('playwright');
 (async () => {
-	const browser = await chromium.launch({headless: false});
+	const browser = await chromium.launch();
 	const context = await browser.newContext();
 	const page = await context.newPage();
 	await page.goto('https://www.ipify.org/');
